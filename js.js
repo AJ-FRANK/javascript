b9afb544f88f7f2d966a515f8a5115f82146c2fc
franklin <ajahfranklyn1@gmail.com>
2024-10-02 07:43:20 -0700

    js prompt assignment

---
+++ js.js
@@ -0,0 +1,10 @@
+function frank() {
+    var name = prompt("hello, welcome to AJFRANK WEBSITE. can you please tell me your name?");
+    var gender = prompt("what is your gender?");
+    var year = prompt("what year were you born?");
+    var age = 2024 - year;
+    var days = year * 365;
+
+    alert("hello " + name + " you are " + " a " + gender + ", you are " + age + " years old " + " and have lived for a period of " + days + " days " + ". can you tell me what you have done with your life all this while?");
+
+}
